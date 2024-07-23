8d39653a88b7de7b566db479d66fd5ab57f6a312
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-23 05:48:32 +0000

    Add functionality to open messages for detailed view

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,13 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
+router.get("/messages/:id", (req, res, next) => {
+  const message = messages.find((msg) => msg.id === req.params.id);
+  if (message) {
+    res.render("message", { message });
+  } else {
+    res.status(404).send("Message not found");
+  }
+});
+
 module.exports = router;
